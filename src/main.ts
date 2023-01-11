f04a357f7f5491432cd6928904a2800fee0f1794
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2023-01-11 10:43:25 +0100

    Finished API documentation with swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -9,6 +10,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('findjob.it')
+    .setDescription('The findjob.it API description')
+    .setVersion('1.0')
+    .addTag('findjob.it')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
